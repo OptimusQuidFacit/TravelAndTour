380aa9d4261a22ed813814e44f5882d29260552d
Kemmy <116153365+OptimusQuidFacit@users.noreply.github.com>
2024-05-16 16:52:34 +0100

    Update summary page to fetch Paystack key from environment variables.

--- src/app/summary/[slug]/page.jsx
+++ src/app/summary/[slug]/page.jsx
@@ -3,13 +3,14 @@ import { auth } from "@/lib/auth";
 import { Suspense } from "react";
 
 const page = async ({params}) => {
+    const paystackKey= process.env.PAYSTACK_KEY;
     const {slug} = params;
     const id= slug.split('%')[0];
     console.log(id);
     const user = (await auth()).user;
     return (
         <div>
-                <Summary id={id} user={user}/>
+                <Summary id={id} user={user} paystackKey={paystackKey}/>
         </div>
     );
 }
