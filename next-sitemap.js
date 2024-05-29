c6c3e6b476684b5de6ebb245cb870f953a76f4bd
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-05-29 13:12:19 +0100

    sitemap and robots.txt configurations

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://ayen-gamma.vercel.app', // Replace with your site's URL
+    exclude: ['/admin/**', '/summary/**'],
+    priority: 0.7
+    // generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // Additional options can be added here
+  };
+  
